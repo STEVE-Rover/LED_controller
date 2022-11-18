124513a2c072b0eca4662a27330ff41b37a2f975
Jérémie Bourque <43643510+JeremieBourque1@users.noreply.github.com>
2022-11-18 14:36:00 -0500

    Moved nav state logic to goal_manager (#2)

--- src/led_controller.py
+++ src/led_controller.py
@@ -15,47 +15,19 @@ class LEDController:
     def __init__(self):
         rospy.init_node("led_controller")
 
-        self.time_thresh = rospy.get_param("~time_thresh", 0.5)
-        self.rate = rospy.get_param("~rate", 5)
-        self.latest_goal_manager_state = (None, rospy.Time(0))  # (Int, Time())
-        self.latest_gamepad_cmd_vel = rospy.Time(0)  # Time()
-        self.led_state = 0  #0: off, 1: solid red, 2: solid blue, 3: flashing green
+        self.led_state = 0  #0: off, 1: solid red, 2: flashing green, 3: solid blue 
         self.prev_led_state = 0
         self.animations = Animations()
 
         rospy.loginfo("led_controller ready")
-        
         rospy.Subscriber("/goal_manager/state", Int8, self.goal_manager_state_cb)
-        rospy.Subscriber("/gamepad_cmd_vel", Twist, self.gamepad_cmd_vel_cb)
 
     def goal_manager_state_cb(self, msg):
-        self.latest_goal_manager_state = (msg.data, rospy.Time.now())
-
-    def gamepad_cmd_vel_cb(self, msg):
-        self.latest_gamepad_cmd_vel = rospy.Time.now()
+        self.led_state = msg.data
 
     def run(self):
         r = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
-            time_since_latest_state = (rospy.Time.now() - self.latest_goal_manager_state[1]).to_sec()
-            time_since_latest_cmd_vel = (rospy.Time.now() -  self.latest_gamepad_cmd_vel).to_sec()
-            if  time_since_latest_cmd_vel < self.time_thresh:
-                # Teleoperation
-                print("Blue light")
-                self.led_state = 2
-            elif time_since_latest_state < self.time_thresh:
-                if self.latest_goal_manager_state[0] == 0:
-                    # Autonomous navigation in progress
-                    print("Red light")
-                    self.led_state = 1
-                elif self.latest_goal_manager_state[0] == 1:
-                    # Goal reached
-                    print("Flashing green light")
-                    self.led_state = 3
-            else:
-                print("No light")
-                self.led_state = 0
-            
             if self.prev_led_state != self.led_state:
                 # LED state has changed.
                 if self.prev_led_state == 3:
@@ -66,10 +38,9 @@ class LEDController:
                 elif self.led_state == 1:
                     self.animations.solid(COLORS[0])
                 elif self.led_state == 2:
-                    self.animations.solid(COLORS[2])
-                elif self.led_state == 3:
                     self.animations.flashing(COLORS[1], 2)
-
+                elif self.led_state == 3:
+                    self.animations.solid(COLORS[2])
             r.sleep()
 
 
